d4cb6f0d5e40d4f3b531093904e4e3e4bf6ba570
Rachid Gaim Ahwaz <s183925@dtu.dk>
2021-01-06 16:43:53 +0100

    Updated VacantField class

--- src/VacantField.java
+++ src/VacantField.java
@@ -1,19 +1,27 @@
-public class VacantField extends PropertyField{
+public class VacantField extends PropertyField {
 
-    int one_house_rent;
-    int two_house_rent;
-    int three_house_rent;
-    int four_house_rent;
+    int[] house_rent;
+    int hotel_rent;
     int house_price;
     int hotel_price;
 
-    public VacantField(String name, String description, int price, int typeIndex, int Rent, int one_house_rent,int two_house_rent, int three_house_rent, int four_house_rent, int house_price, int hotel_price) {
+    public VacantField(String name, String description, int price, int typeIndex, int Rent, int one_house_rent, int two_house_rent, int three_house_rent, int four_house_rent, int hotel_rent, int house_price, int hotel_price) {
         super(name, description, price, typeIndex, Rent);
-        this.one_house_rent = one_house_rent;
-        this.two_house_rent = two_house_rent;
-        this.three_house_rent = three_house_rent;
-        this.four_house_rent = four_house_rent;
+        this.house_rent = new int[]{one_house_rent, two_house_rent, three_house_rent, four_house_rent};
+        this.hotel_rent = hotel_rent;
         this.house_price = house_price;
         this.hotel_price = hotel_price;
+    }
+
+    public int getrent(int value) {
+        if (value == 0) {
+            return Rent;
+        }
+        if (value > 0 && value < 5) {
+            return house_rent[value];
+        }
+        else {
+            return hotel_rent;
+        }
     }
 }
