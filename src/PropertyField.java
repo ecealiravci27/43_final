4834b3931fe77c8fce291f01194ccee2c950f878
Rachid Gaim Ahwaz <s183925@dtu.dk>
2021-01-06 15:51:18 +0100

    Made abstract class PropertyField

--- src/PropertyField.java
+++ src/PropertyField.java
@@ -1,2 +1,15 @@
-public class PropertyField {
+public abstract class PropertyField extends SuperField{
+
+    public int price;
+    public String Color;
+    public int Rent;
+    String[] Colors = new String[]{ "Blue","Orange", "light-yellow", "purple", "gray", "red", "white", "yellow"};
+
+    public PropertyField(String name, String description, int price,int typeIndex, int Rent) {
+        super(name, description);
+        this.price = price;
+        this.Color = Colors[typeIndex];
+        this.Rent = Rent;
+    }
+
 }
