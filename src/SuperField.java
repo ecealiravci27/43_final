4963c7a9a33393bb56d9eb9f45947eca1c519373
Rachid Gaim Ahwaz <s183925@dtu.dk>
2021-01-06 15:22:32 +0100

    Made Superfield class

--- src/SuperField.java
+++ src/SuperField.java
@@ -1,2 +1,16 @@
-public class SuperField {
+public abstract class SuperField {
+    protected String fieldName;
+    protected String fieldDescription;
+
+    public SuperField(String name, String description){
+        this.fieldName = "";
+        this.fieldDescription = "";
+    }
+
+    public String getFieldName() {
+        return fieldName;
+    }
+    public String getFieldDescription(){
+        return fieldDescription;
+    }
 }
