6fd73698903ce544f9918fb56ca624fe5c831e85
Zeinab <you@example.com>
2021-01-06 10:37:19 +0100

    Worked on dice

--- src/Dice.java
+++ src/Dice.java
@@ -1,2 +1,27 @@
+
 public class Dice {
+    int diceOutcome;
+    int temp;
+
+    public Dice(){
+        this.diceOutcome = 0;
+    }
+    public int rollDice() {
+        int maxEye = 6;
+        int minEye = 1;
+        temp = (int) ((Math.random() * maxEye) + minEye);
+        return temp;
+    }
+    public void rememberDice(int diceOutcome) {
+        this.diceOutcome = diceOutcome;
+    }
+
+    public int getRememberDice(){
+        return diceOutcome;
+    }
 }
+
+    public static int diceTotal() {
+        int roll1 = rollDice();
+        int roll2 = rollDice();
+        return roll1 + roll2;
