09d8d59bfeb0c776f49c023e0ed8347868a0495f
ecealiravci27 <s205433@student.dtu.dk>
2021-01-06 10:35:52 +0100

    Created pieces and colors

--- src/Piece.java
+++ src/Piece.java
@@ -1,2 +1,29 @@
 public class Piece {
+    String pieceColor;
+    int totalMoves;
+    String[] Color = new String[]{"Red", "Blue", "Green", "Yellow", "Orange", "Purple"};
+
+    public Piece(){
+        this.totalMoves = 0;
+    }
+
+    public void movePiece(int addMove) {
+        totalMoves = totalMoves + addMove;
+    }
+
+    public void setTotalMoves(int position){
+        totalMoves = position;
+    }
+
+    public int getMoves() {
+        return totalMoves;
+    }
+    public String getColor(){
+        return pieceColor;
+    }
+
+    public String[] setColor(String[] pieceColor){
+        Color = pieceColor;
+        return Color;
+    }
 }
