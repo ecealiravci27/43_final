bfd797edbabcfd24ff4ca6de762b577219737f48
Rachid Gaim Ahwaz <s183925@dtu.dk>
2021-01-07 15:35:41 +0100

    Made SpecialField and ChanceField

--- src/Board.java
+++ src/Board.java
@@ -1,2 +1,6 @@
 public class Board {
+
+    private SuperField[] fields;
+    private int totalFields;
+
 }
--- src/SpecialField.java
+++ src/SpecialField.java
@@ -1,2 +1,19 @@
-public class SpecialField {
+public class SpecialField extends SuperField{
+
+    private final boolean goToJail;
+    private final boolean payTaxes;
+
+    public SpecialField(String name, String description, int ID,boolean goToJail, boolean payTaxes) {
+        super(name, description, ID);
+        this.goToJail = goToJail;
+        this.payTaxes = payTaxes;
+    }
+
+    public boolean isGOtoJail(){
+        return goToJail;
+    }
+    public boolean isPayTaxes(){
+        return payTaxes;
+    }
+
 }
