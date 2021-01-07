79af9b3def8fbbc243906af51b423d0a2e0b7701
Muaz <muaz@outlook.dk>
2021-01-07 17:48:44 +0100

    Finished logic for CardController and sub cards.

---
+++ src/CardController.java
@@ -0,0 +1,18 @@
+public class CardController {
+    // Cardpile test
+    public static void main(String[] args) {
+        SuperCard card = new CardPile().drawCard();
+        System.out.println(card);
+        if (MoneyCard.class.isInstance(card)) {
+            System.out.println(("Charging money: " + ((MoneyCard) card).getChangeMoney()));
+        } else if(MoveCard.class.isInstance(card)) {
+            if(((MoveCard) card).getType() == 1) {
+                // movepiece here
+                System.out.println("Moving piece " + ((MoveCard) card).getMovePiece());
+            } else {
+                // movefield here
+                System.out.println("Moving field " + ((MoveCard) card).getMoveToField());
+            }
+        }
+    }
+}
