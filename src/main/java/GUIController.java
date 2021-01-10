8d71895e16ab0f2f7d12f5281f598f710e74eaa5
Muaz <muaz@outlook.dk>
2021-01-10 15:22:05 +0100

    GUIController now gets most information from board

--- src/main/java/GUIController.java
+++ src/main/java/GUIController.java
@@ -1,7 +1,5 @@
-import Fields.PropertyField;
-import Fields.SuperField;
-import gui_fields.GUI_Field;
-import gui_fields.GUI_Street;
+import Fields.*;
+import gui_fields.*;
 import gui_main.GUI;
 
 import java.awt.*;
@@ -14,11 +12,29 @@ public class GUIController {
         for (int i = 0; i < board.length; i++) {
             field = board[i];
             if(field instanceof Fields.SpecialField) {
-                //gfields[i] = new GUI_
-            } else if(field instanceof Fields.PropertyField) {
-                int rent = ((PropertyField) field).getFieldRent();
-                gfields[i] = new GUI_Street(field.getFieldName(), "subtext", field.getFieldDescription(), String.valueOf(rent), Color.BLUE, Color.BLACK);
+                if(((SpecialField) field).getType() == "start") {
+                    gfields[i] = new GUI_Start(field.getFieldName(), "subtext", field.getFieldDescription(), Color.ORANGE, Color.WHITE);
+                } else if(((SpecialField) field).getType() == "tax") {
+                    gfields[i] = new GUI_Tax(field.getFieldName(), "subtext", field.getFieldDescription(), Color.BLACK, Color.WHITE);
+                } else if(((SpecialField) field).getType() == "visitprison" || ((SpecialField) field).getType() == "prison") {
+                    gfields[i] = new GUI_Jail("default", field.getFieldName(), "subtext", field.getFieldDescription(), Color.BLACK, Color.WHITE);
+                } else if(((SpecialField) field).getType() == "parking") {
+                    gfields[i] = new GUI_Refuge("default", field.getFieldName(), "subtext", field.getFieldDescription(), Color.BLACK, Color.BLUE);
+                }
+
+            } else if(field instanceof Fields.VacantField) {
+                int rent = ((VacantField) field).getFieldRent();
+                System.out.println(String.valueOf(rent));
+                gfields[i] = new GUI_Street(field.getFieldName(), "subtext", field.getFieldDescription(), String.valueOf(rent), Color.LIGHT_GRAY, Color.BLACK);
                 //new GUI_Street(,)
+            } else if(field instanceof Fields.ChanceField) {
+                gfields[i] = new GUI_Chance(field.getFieldName(), "subtext", field.getFieldDescription(), Color.GREEN, Color.black);
+            } else if(field instanceof Fields.ShippingField) {
+                int rent = ((ShippingField) field).getFieldRent();
+                gfields[i] = new GUI_Shipping("default", field.getFieldName(), "subtext", field.getFieldDescription(), String.valueOf(rent), Color.ORANGE, Color.BLACK);
+            } else if(field instanceof Fields.CoorporationField) {
+                int rent = ((CoorporationField) field).getFieldRent();
+                gfields[i] = new GUI_Brewery("default", field.getFieldName(), "subtext", field.getFieldDescription(), String.valueOf(rent), Color.blue, Color.orange);
             }
         }
         return gfields;
@@ -26,6 +42,7 @@ public class GUIController {
 
     public static void main(String[] args) {
         SuperField[] board = new Board().setupField();
+        //System.out.println(setupBoard(board));
         GUI gui = new GUI(setupBoard(board));
     }
 }
