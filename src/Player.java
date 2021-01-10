ba223a9541c3f48cdbb213aa5a29a5618a6ce54a
JensHOls <s201729@student.dtu.dk>
2021-01-10 12:58:19 +0100

    Added attributes and basic methods to Player. Added class PropertyPlayerController

--- src/Player.java
+++ src/Player.java
@@ -1,4 +1,24 @@
 public class Player {
 
+    private int playerPosition;
+    private int playerID;
+    private Piece playerPiece;
+    private Account playerAccount;
+
+    public Player(int playerNumber) {
+        this.playerID = playerNumber;
+        this.playerPiece = new Piece(playerNumber);
+        this.playerPosition = 0;
+        this.playerAccount = new Account(30000);
+    }
+
+    public void movePiece(int addMove) {
+        playerPiece.movePiece(addMove);
+    }
+
+    public int getMoves() {
+
+        return playerPiece.getMoves();
+    }
 
 }
