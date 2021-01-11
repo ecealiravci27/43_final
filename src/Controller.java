2627d76b50365284edf7df3f622c7dde2e2f1fdd
Rachid Gaim Ahwaz <s183925@dtu.dk>
2021-01-11 11:34:34 +0100

    worked on controller

--- src/Controller.java
+++ src/Controller.java
@@ -18,15 +18,13 @@ public class Controller {
 
         for (int i = 0; i < 5; i++) {
 
-            for (int j = 1; j < 2; j++) {
+            for (int j = 1; j < 3; j++) {
 
                 propertyPlayerController.movePiece(dice.calculateEyeSum(), j);
                 System.out.println("Player " + j + " eye sum " + dice.getDiceOutcome());
                 propertyPlayerController.getPlayerPosition(j);
                 System.out.println("Player " + j + " position : " + propertyPlayerController.getPlayerPosition(j));
-
             }
-
         }
     }
 
