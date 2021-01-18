baa5cbc8f2296d8259374510dbbca88a8551a2b3
Muaz <muaz@outlook.dk>
2021-01-18 10:56:10 +0100

    GUI Comments

--- src/main/java/GUI/GUIController.java
+++ src/main/java/GUI/GUIController.java
@@ -20,10 +20,12 @@ public class GUIController {
         options = playerColors.keySet().toArray(new String[0]);
     }
 
+    // Creates all GUI fields from information that it gets from board class
     public GUI_Field[] setupBoard(SuperField[] board) {
         GUI_Field[] gfields = new GUI_Field[board.length];
         SuperField field;
 
+        // Hashmap with colors, FieldColor is a class that is able to store two colors, FG and BG.
         HashMap <String, FieldColor> colors = new HashMap<>();
 
         Color cRed = new Color(229, 62,62);
@@ -61,6 +63,7 @@ public class GUIController {
         // Shipping
         colors.put("shipping", new FieldColor(cWhite, cBlack));
 
+        // Vacantfield
         colors.put("vacant.0", new FieldColor(cBlue, cBlack));
         colors.put("vacant.1", new FieldColor(cOrange, cBlack));
         colors.put("vacant.2", new FieldColor(cSlate, cWhite));
@@ -70,7 +73,7 @@ public class GUIController {
         colors.put("vacant.6", new FieldColor(cBeige, cBlack));
         colors.put("vacant.7", new FieldColor(cTeal, cBlack));
 
-        //colors.put()
+        // Starts creating the fields
         for (int i = 0; i < board.length; i++) {
             field = board[i];
             if(field instanceof Model.Fields.SpecialField) {
@@ -129,10 +132,12 @@ public class GUIController {
         return gfields;
     }
 
+    // Shows two dices that display the eye for each roll
     public void showDice(int roll, int roll2) {
         GUI.setDice(roll, roll2);
     }
 
+    // Dropdown menu for how many players are in the game. Currently 2 minimum and 6 maximum. Dynamic information that it gets from the controller.
     public int totalplayers(int min, int max) {
         String[] options = new String[max - min + 1];
 
@@ -147,6 +152,7 @@ public class GUIController {
         return Integer.parseInt(chosenElement);
     }
 
+    // Creates a text field where the player can name themselves. Checks if the name is already taken or if the name is empty and calls itself if needed.
     public String setupName(String txt) {
         String chosenName = GUI.getUserString(txt);
         String lowercase = chosenName.toLowerCase();
@@ -158,15 +164,17 @@ public class GUIController {
         return chosenName;
     }
 
+    // Creates the individual color that you choose when you select the color you want in the dropdown menu after totalplayers.
     public GUI_Car setupCar(Color color) {
         return new GUI_Car(color, color, GUI_Car.Type.CAR, GUI_Car.Pattern.FILL);
     }
 
+    // Removes an option from a array. This is used when a player has chosen a color.
     public String[] popElement(String element, String[] array) {
         int counter = 0;
         boolean found = false;
 
-        // if element doesn't exist
+        // Check if element exists
         for (String string: array) {
             if(string.equals(element)) {
                 found = true;
@@ -185,6 +193,7 @@ public class GUIController {
         return newArray;
     }
 
+    // Creates the GUI player object
     public GUI_Player[] GUIPlayers(Player[] players) {
         gPlayers = new GUI_Player[players.length];
         for (int i = 0; i < players.length; i++) {
@@ -197,6 +206,7 @@ public class GUIController {
         return gPlayers;
     }
 
+    // Change position on the individual cars
     public void changePlayerGUIPos(int playerID, int newpos, int oldpos) {
         GUI.getFields()[oldpos].setCar(gPlayers[playerID], false);
         GUI.getFields()[newpos].setCar(gPlayers[playerID], true);
@@ -204,6 +214,7 @@ public class GUIController {
         System.out.printf("Changed guipos from: " + oldpos + " to newpos: " +newpos);
     }
 
+    // HashMap of color options for the player
     public HashMap<String, Color> setupPlayerColors() {
         HashMap <String, Color> colors = new HashMap<>();
 
@@ -217,6 +228,7 @@ public class GUIController {
         return colors;
     }
 
+    // Dropdown menu for player color
     public Color choosePlayerColor() {
         String chosenElement = GUI.getUserSelection(
                 "Vælg farve",
@@ -226,7 +238,7 @@ public class GUIController {
         return playerColors.get(chosenElement);
     }
 
-
+    // Asks the player if they want to buy a property
     public boolean wantToBuy(String propertyname) {
         String chosenButton = GUI.getUserButtonPressed(
                 "Vil du gerne købe: " + propertyname,
@@ -235,6 +247,7 @@ public class GUIController {
         return chosenButton.equals("Ja");
     }
 
+    // Tells the player who's turn it is and gives a roll button
     public void wantToRoll(int ID) {
         GUI.getUserButtonPressed(
                 gPlayers[ID].getName() + "'s tur",
@@ -242,6 +255,7 @@ public class GUIController {
         );
     }
 
+    // Dynamic message that can be used to display information that the player needs to acknowledge
     public void message(String msg) {
         GUI.getUserButtonPressed(
                 msg,
@@ -249,6 +263,7 @@ public class GUIController {
         );
     }
 
+    // Used to set the border of a propertyfield that a player buys
     public void setPropertyBorder(int playerID, int fieldID) {
         GUI_Field field = GUI.getFields()[fieldID];
         if (field instanceof GUI_Shipping) {
@@ -259,6 +274,8 @@ public class GUIController {
             ((GUI_Brewery) field).setBorder(gPlayers[playerID].getPrimaryColor());
         }
     }
+
+    // Removes the border we set on setPropertyBorder, used when a player goes bankrupt
     public void removePlayerBorder(int playerID, int fieldID) {
         Color cBlack = new Color(0, 0, 0);
         GUI_Field field = GUI.getFields()[fieldID];
@@ -275,22 +292,18 @@ public class GUIController {
         gPlayers[playerID].setBalance(balance);
     }
 
+    // Gets a array of fields that it needs to a dropdown menu
     public VacantField wantToBuildHouse(VacantField[] fields) {
         String[] names = new String[fields.length + 1];
         VacantField chosenField = null;
 
         System.out.println(" Fields.length : " + fields.length);
 
+        // Add the objects names to string array
         for (int i = 0; i < fields.length; i++) {
-            //if (fields[i] == null) continue;
             System.out.println(" fields[i] " + fields[i]);
             System.out.println(" fields[i]" + fields[i].getFieldName());
             names[i] = fields[i].getFieldName();
-        }
-
-        for (int i = 0; i < names.length; i++) {
-            System.out.println("index = " + i);
-            System.out.println("navn = " + names[i]);
         }
 
         names[names.length - 1] = "AFBRYD";
@@ -300,12 +313,15 @@ public class GUIController {
                 names
         );
 
+        // Gets the field object that we have chosen from chosenElement
         for (int i = 0; i < fields.length; i++) {
             if (chosenElement.equals(fields[i].getFieldName())) {
                 chosenField = fields[i];
                 break;
             }
         }
+
+        // returns null if chosenElement = AFBRYD
         return chosenField;
     }
 
